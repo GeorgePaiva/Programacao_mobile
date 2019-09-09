6eb256615ab99aed93e4af168443e0e4dd93b60b
GeorgePaiva <george.paiva15@gmail.com>
2019-09-09 20:29:36 -0300

    Adicionando formulario

---
+++ App-FAC-master/app/src/main/java/com/example/ads052dacio/CriarAlunoActivity.java
@@ -0,0 +1,14 @@
+package com.example.ads052dacio;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CriarAlunoActivity extends AppCompatActivity {
+    @Override
+    protected void OnCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_criar_alunos);
+    }
+
+}
---
+++ App-FAC-master/app/src/main/java/com/example/ads052dacio/ListAlunosActivity.java
@@ -0,0 +1,15 @@
+package com.example.ads052dacio;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ListAlunosActivity extends AppCompatActivity {
+
+    @Override
+    protected void OnCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_alunos);
+    }
+
+}
