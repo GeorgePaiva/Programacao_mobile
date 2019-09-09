cf9e15ba226432158315a7e371e9e72f6e177d1f
GeorgePaiva <george.paiva15@gmail.com>
2019-09-09 20:52:02 -0300

    Colocando onClick no botao

--- App-FAC-master/app/src/main/java/com/example/ads052dacio/CriarAlunoActivity.java
+++ App-FAC-master/app/src/main/java/com/example/ads052dacio/CriarAlunoActivity.java
@@ -1,6 +1,7 @@
 package com.example.ads052dacio;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,6 +10,10 @@ public class CriarAlunoActivity extends AppCompatActivity {
     protected void OnCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_criar_alunos);
+    }
+
+    public void cadastrarAlunoBtn(View view){
+        System.out.println("Olá, você clicou no botão cadastrar!");
     }
 
 }
